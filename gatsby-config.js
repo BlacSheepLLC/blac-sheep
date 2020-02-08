698ff24ef7d7377c04a6a0b4b28de1772ddc5ec9
Luis Fernando Saavedra Meza <fernando.saavedra@blac-sheep.com>
2020-02-08 17:28:38 -0600

    Pin toolchain and update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Blac—Sheep`,
+    description: `Web and mobile app development studio that partners with startups to build amazing products.`,
+    author: `@blac-sheep`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,

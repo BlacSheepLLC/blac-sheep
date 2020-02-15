6d7833f5c195a2063fd176113961cbfc12b2ffff
Luis Fernando Saavedra Meza <fernando.saavedra@blac-sheep.com>
2020-02-14 21:18:27 -0600

    Implement services section

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,25 +5,46 @@ import SEO from '../components/SEO';
 
 import styles from './index.module.css';
 
-console.log(styles);
-
 const IndexPage = () => (
   <Layout>
     <SEO title="Development Studio" />
 
-    <header className={`${styles.container} ${styles.oHeroHeader}`}>
-      <h1 className={`${styles.oHeroTitle}`}>
-        We&apos;re a web and mobile app development studio
-      </h1>
-      <p className={`${styles.oHeroLead}`}>
-        We design and develop the next generation of digital products by
-        partnering with startups as their remote in-house development team.
-      </p>
+    <div className={styles.home}>
+      <header className={`${styles.container} ${styles.heroHeader}`}>
+        <h1 className={`${styles.heroTitle}`}>We&apos;re a web and mobile app development studio</h1>
+        <p className={`${styles.heroLead}`}>
+          We partner with startups as a remote in-house product development team — providing the management, design and
+          development workforce to meet your goals
+        </p>
 
-      <a href="#contact" className={`${styles.oHeroCta} o-interactive-cta`}>
-        Curious yet?
-      </a>
-    </header>
+        <a href="#contact" className={`${styles.heroCta} o-interactive-cta`}>
+          Curious yet?
+        </a>
+      </header>
+
+      <section className={`${styles.container} ${styles.servicesSection}`}>
+        <span className={`${styles.subtitle}`}>Our services</span>
+        <h2 className={`${styles.title}`}>
+          We play the long run by delivering high-quality, user-fiendly products that people love to use
+        </h2>
+
+        <dl className={`${styles.serviceList}`}>
+          <dt>Web</dt>
+          <dd>
+            Be it a marketing site, a blog, an e-commerce platform or a full-blown web application we&apos;ll ensure
+            your site or application runs smoothly across browsers, screen sizes and even device types
+          </dd>
+          <dd>UI / UX design · Prototyping · Frontend · Backend</dd>
+
+          <dt>Mobile apps</dt>
+          <dd>
+            iOS & Android — hate one or the other, love them both. We tailor each platform&apos;s uniqueness to deliver
+            a fast, responsive and unified cross-platform experience for your app
+          </dd>
+          <dd>UI / UX design · Prototyping · iOS · Android · Hybrid apps · Backend</dd>
+        </dl>
+      </section>
+    </div>
   </Layout>
 );
 

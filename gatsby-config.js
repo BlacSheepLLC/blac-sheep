d64614860fa89886865c6053045cb60815bebe94
Luis Fernando Saavedra Meza <fernando.saavedra@blac-sheep.com>
2020-02-22 19:02:16 -0600

    Enable PWA's offline capabilities

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,18 +18,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Blac—Sheep`,
+        short_name: `Blac—Sheep`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#ffffff`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-offline`,
     `gatsby-plugin-eslint`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 };

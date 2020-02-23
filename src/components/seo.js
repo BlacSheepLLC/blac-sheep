01438e73524aa23567a1aa0a4ecb0989f1889d10
Luis Fernando Saavedra Meza <fernando.saavedra@blac-sheep.com>
2020-02-22 18:53:14 -0600

    Remove user-scalable viewport attribute

--- src/components/seo.js
+++ src/components/seo.js
@@ -37,7 +37,7 @@ function SEO({ description, lang, meta, title }) {
       meta={[
         {
           name: `viewport`,
-          content: `width=device-width, initial-scale=1, user-scalable=no, shrink-to-fit=no, viewport-fit=cover`,
+          content: `width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover`,
         },
         {
           name: `description`,
